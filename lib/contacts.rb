0b0b87e42dc2072a9dcbc9f58507da02a81811fd
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-14 15:57:19 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,6 @@ contacts = {
 
 
 def remove_strawberry(contacts)
-  def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
