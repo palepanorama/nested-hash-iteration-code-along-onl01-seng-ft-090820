e6a3bbd969e0979f5dff6d9f150a735e9e2b2600
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-14 15:52:10 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -12,13 +12,14 @@ contacts = {
     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   }
 }
-
-def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       binding.pry
     end
   end
+
+def remove_strawberry(contacts)
+
 end
   
